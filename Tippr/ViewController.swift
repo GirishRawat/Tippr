c5daa10ac8f1fd60429f2e430a6fde801416d9ec
Girish Rawat <girishrawat@Girishs-MacBook-Air.local>
2017-01-05 23:45:19 +0530

    Added .gitignore and .gitattributes

---
+++ Tippr/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Tippr
+//
+//  Created by Girish Rawat on 1/5/17.
+//  Copyright © 2017 Girish Rawat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+    @IBAction func onTap(_ sender: Any) {
+        //print("Hello")
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.20, 0.22]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        tipLabel.text = String(format: "$%.2f" , tip)
+        totalLabel.text = String(format: "$%.2f" , total)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+// This is a comment
+// Comments are useful in determining or writing code that won't be executed
+
---
+++ TipprTests/TipprTests.swift
@@ -0,0 +1,36 @@
+//
+//  TipprTests.swift
+//  TipprTests
+//
+//  Created by Girish Rawat on 1/5/17.
+//  Copyright © 2017 Girish Rawat. All rights reserved.
+//
+
+import XCTest
+@testable import Tippr
+
+class TipprTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
